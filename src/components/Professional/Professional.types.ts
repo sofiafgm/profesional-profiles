84f9d6ed02851f8a0d92938fad0a72f63a3d4613
Omarcaste7 <omar.garcia1039@alumnos.udg.mx>
2026-08-07 22:10:28 -0600

    Add ProfessionalProps interface for component props

--- src/components/Professional/Professional.types.ts
+++ src/components/Professional/Professional.types.ts
@@ -1,1 +1,14 @@
-
+export interface ProfessionalProps {
+  id?: number;
+  name: string;
+  cedula: string;
+  institution: string;
+  approach: string;
+  population: string;
+  areas: string;
+  experience: string;
+  modality: string;
+  schedule: string;
+  message: string;
+  imageUrl: string;
+}
